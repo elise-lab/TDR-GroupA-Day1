d019e44daf59b77c3f4a8df770ce91434082d5f1
quialst <blueoceanquincy@gmail.com>
2023-06-30 11:00:17 -0700

    Revert "reconfig no longer repeats points"
    This reverts commit fc3d2bc9273860211b785c10709165a2790cdff9.

--- TDR Project 2/Jupyter Notebooks/reconfig.py
+++ TDR Project 2/Jupyter Notebooks/reconfig.py
@@ -1,12 +1,12 @@
 import xlrd
 
-workbook = xlrd.open_workbook(r'genus 3.xls')
+workbook = xlrd.open_workbook(r'sphere.xls')
 worksheet = workbook.sheet_by_name('Sheet 1')
 
-with open('data/genus 3 clean.txt', 'w+') as f:
+with open('data/sphere clean.txt', 'w+') as f:
     for i in range(worksheet.nrows-1):
         for j in range(4):
             f.write(worksheet.cell(i, j).value)
             f.write(', ')
-        f.write(worksheet.cell(i, 4).value)
+        f.write(worksheet.cell(i, 3).value)
         f.write('\n')
