c545619fbcbb5c95c290d3ebe3dd43b0a6d24ecf
quialst <blueoceanquincy@gmail.com>
2023-07-03 17:43:33 -0700

    added degen pentagons to definitions

--- TDR Project 2/Jupyter Notebooks/definitions.py
+++ TDR Project 2/Jupyter Notebooks/definitions.py
@@ -70,6 +70,27 @@ def max_sum_angle_op(dist, start_ang): #Take in the distances of our pentagon an
     except ValueError:
         raise ValueError('input new starting angle')
 
+def standard_form(penta):
+    #penta should be list of pentagons, so there is nested list notation
+    list_stand_frm = []
+    size = len(penta)
+    for i in range(size):
+        c = penta[i][0] - penta[i][8]
+        d = penta[i][1] - penta[i][9]
+        #call a pentagon, its vertices specifically
+        # apply the formula and create the standard form
+        w1 = (((c*(penta[i][2] - penta[i][8])) + d*(penta[i][3] - penta[i][9]))/(c**2 + d**2), 
+((-d*(penta[i][2] - penta[i][8])) + c*(penta[i][3] - penta[i][9]))/(c**2 + d**2))
+        
+        w2 = (((c*(penta[i][4] - penta[i][8])) + d*(penta[i][5] - penta[i][9]))/(c**2 + d**2), 
+        ((-d*(penta[i][4] - penta[i][8])) + c*(penta[i][5] - penta[i][9]))/(c**2 + d**2))
+        
+        w3 = (((c*(penta[i][6] - penta[i][8])) + d*(penta[i][7] - penta[i][9]))/(c**2 + d**2), 
+        ((-d*(penta[i][6] - penta[i][8])) + c*(penta[i][7] - penta[i][9]))/(c**2 + d**2))
+        
+        list_stand_frm.append([1, w1, w2, w3, 0])
+        
+    return list_stand_frm
 
 
 #first we define a function which takes theta1,theta2,(d_2,d_3,d_4,d_5) assuming d_1=1 and outputs a list [(a,b),...]
@@ -175,24 +196,52 @@ def moduli_space_sample(n, distances, start_ang, sampling='rd'):
         
     return sample
 
-def standard_form(penta):
-    #penta should be list of pentagons, so there is nested list notation
-    list_stand_frm = []
-    size = len(penta)
-    for i in range(size):
-        c = penta[i][0] - penta[i][8]
-        d = penta[i][1] - penta[i][9]
-        #call a pentagon, its vertices specifically
-        # apply the formula and create the standard form
-        w1 = (((c*(penta[i][2] - penta[i][8])) + d*(penta[i][3] - penta[i][9]))/(c**2 + d**2), 
-((-d*(penta[i][2] - penta[i][8])) + c*(penta[i][3] - penta[i][9]))/(c**2 + d**2))
-        
-        w2 = (((c*(penta[i][4] - penta[i][8])) + d*(penta[i][5] - penta[i][9]))/(c**2 + d**2), 
-        ((-d*(penta[i][4] - penta[i][8])) + c*(penta[i][5] - penta[i][9]))/(c**2 + d**2))
-        
-        w3 = (((c*(penta[i][6] - penta[i][8])) + d*(penta[i][7] - penta[i][9]))/(c**2 + d**2), 
-        ((-d*(penta[i][6] - penta[i][8])) + c*(penta[i][7] - penta[i][9]))/(c**2 + d**2))
-        
-        list_stand_frm.append([1, w1, w2, w3, 0])
-        
-    return list_stand_frm
+
+def degenerate_pent(n, distances):
+    if sum(distances)<1:
+        raise InternalException('Distances too small to make a valid pentagon.')
+    
+    output=[]
+    #right oriented degenerate pentagons
+    max_angle=angle_from_side(distances[0], distances[2]+(1-distances[3]),distances[1])
+    theta2=np.linspace(0,max_angle,n)
+    for i in theta2:
+        w3=[1-distances[0]*np.cos(i),distances[0]*np.sin(i)]
+        w4=get_intersections(distances[3],0, distances[2], w3[0], w3[1], distances[1])
+        if type(w4)==str:
+            continue
+        w4=[x for x in w4 if x[1]==min(w4[0][1],w4[1][1])][0]#take intersection point below x axis
+        p=[[0,0],[1,0],w3,w4,[distances[3],0]]
+        output.append(p)
+        p=[[0,0],[1,0],[w3[0],-1*w3[1]],[w4[0],-1*w4[0]],[distances[3],0]]#flipped orientation
+        output.append(p)
+    
+    #left oriented degenerate pentagons
+    max_angle=angle_from_side(distances[1]+(1-distances[0]), distances[3],distances[2])
+    theta2=np.linspace(0,max_angle,n)
+    for i in theta2:
+        w5=[distances[3]*np.cos(i),distances[3]*np.sin(i)]
+        w4=get_intersections(w5[0], w5[1], distances[2], 1-distances[0], 0, distances[1])
+        if type(w4)==str:
+            continue
+        w4=[x for x in w4 if x[1]==min(w4[0][1],w4[1][1])][0]#take intersection point below x axis
+        p=[[0,0],[1,0],[1-distances[0],0],w4,w5]
+        output.append(p)
+        p=[[0,0],[1,0],[1-distances[0],0],[w4[0],-1*w4[1]],[w5[0],-1*w5[1]]]#flipped orientation
+        output.append(p)
+    
+    #center oriented degenerate pentagons
+    max_angle=angle_from_side(distances[0]+(max(distances[1],distances[2])-distances[0]),distances[3],1)
+    theta2=np.linspace(0,max_angle,n)
+    for i in theta2:
+        w1=[distances[3]-distances[3]*np.cos(i),distances[3]*np.sin(i)]
+        w2=get_intersections(w1[0], w1[1], 1, distances[0], 0, distances[0])
+        if type(w2)==str:
+            continue
+        w2=[x for x in w2 if x[1]==min(w2[0][1],w2[1][1])][0]#take intersection point below x axis
+        p=[w1,w2,[distances[2],0],[0,0],[distances[3],0]]
+        output.append(p)
+        p=[[w1[0],w1[1]],[w2[0],w2[1]],[distances[2],0],[0,0],[distances[3],0]]#flipped orientation
+        output.append(p)
+
+    return np.array(output,dtype=object)
