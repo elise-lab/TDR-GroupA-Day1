364c488de10e02195e949f54f8fb822994ae5bf7
quialst <blueoceanquincy@gmail.com>
2023-07-05 10:22:41 -0700

    added m5

--- TDR Project 2/Jupyter Notebooks/reconfig.py
+++ TDR Project 2/Jupyter Notebooks/reconfig.py
@@ -1,9 +1,9 @@
 import xlrd
 
-workbook = xlrd.open_workbook(r'genus 3 degen.xls')
+workbook = xlrd.open_workbook(r'M5.xls')
 worksheet = workbook.sheet_by_name('Sheet 1')
 
-with open('data/genus 3 degen clean.txt', 'w+') as f:
+with open('data/M5 clean.txt', 'w+') as f:
     for i in range(worksheet.nrows-1):
         for j in range(4):
             f.write(worksheet.cell(i, j).value)
