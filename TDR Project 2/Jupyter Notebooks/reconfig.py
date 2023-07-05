5eade4b224c61875251341fbf152df99ae5d10d5
quialst <blueoceanquincy@gmail.com>
2023-07-05 14:28:40 -0700

    all new data

--- TDR Project 2/Jupyter Notebooks/reconfig.py
+++ TDR Project 2/Jupyter Notebooks/reconfig.py
@@ -1,9 +1,9 @@
 import xlrd
 
-workbook = xlrd.open_workbook(r'M5 new.xls')
+workbook = xlrd.open_workbook(r'spinning top.xls')
 worksheet = workbook.sheet_by_name('Sheet 1')
 
-with open('data/M5 new clean.txt', 'w+') as f:
+with open('data/spinning top clean.txt', 'w+') as f:
     for i in range(worksheet.nrows-1):
         for j in range(4):
             f.write(worksheet.cell(i, j).value)
