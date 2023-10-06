27e72284038cb5fcb9c931f4b82ecd3e88a5ce7b
quialst <blueoceanquincy@gmail.com>
2023-10-06 18:14:56 +0900

    genus 1 added

--- TDR Project 2/Jupyter Notebooks/reconfig.py
+++ TDR Project 2/Jupyter Notebooks/reconfig.py
@@ -1,9 +1,9 @@
 import xlrd
 
-workbook = xlrd.open_workbook(r'G3large.xls')
+workbook = xlrd.open_workbook(r'G2.xls')
 worksheet = workbook.sheet_by_name('Sheet 1')
 
-with open('1. data/G3_large.txt', 'w+') as f:
+with open('1. data/G2.txt', 'w+') as f:
     for i in range(worksheet.nrows-1):
         for j in range(4):
             f.write(worksheet.cell(i, j).value)
